09a91e0ff7bce718876f916ff4c11213a1298cf2
Xin Wen Yap <yapxinwen1608@gmail.com>
2019-07-15 17:52:22 +0900

    tbc

--- app.rb
+++ app.rb
@@ -1,7 +1,12 @@
-require "sinatra"
-require "sinatra/reloader" if development?
-require "pry-byebug"
-require "better_errors"
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'pry-byebug'
+require 'better_errors'
+
+require_relative '../../01-Cookbook-Advanced/lib/cookbook.rb'
+require_relative '../../01-Cookbook-Advanced/lib/recipe.rb'
+
+set :bind, '0.0.0.0'
 
 configure :development do
   use BetterErrors::Middleware
@@ -9,5 +14,11 @@ configure :development do
 end
 
 get '/' do
-  'Hello world!'
+  @cookbook = Cookbook.new('../../01-Cookbook-Advanced/lib/recipes.csv')
+  @recipes = @cookbook.recipes
+  erb :index
+end
+
+get '/new' do
+  erb :new
 end
